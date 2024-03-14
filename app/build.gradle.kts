daa646e67d634a06fe1101d450437af00d8d161c
yasuhiroki <yasuhiroki.duck@gmail.com>
2024-03-15 00:30:22 +0900

    testing lifecycle version 2.7.0 LiveData map initlai event

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -54,7 +54,9 @@ dependencies {
     implementation("androidx.core:core-ktx:1.9.0")
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.11.0")
-    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.7.0")
+    val lifecycleVersion = "2.7.0"
+    implementation("androidx.lifecycle:lifecycle-livedata-ktx:$lifecycleVersion")
+    implementation("androidx.lifecycle:lifecycle-runtime-ktx:$lifecycleVersion")
     implementation("androidx.activity:activity-compose:1.8.2")
     implementation(platform("androidx.compose:compose-bom:2023.03.00"))
     implementation("androidx.compose.ui:ui")
--- app/src/main/java/jp/yasuhiroki/enjoyandroid/MainActivity.kt
+++ app/src/main/java/jp/yasuhiroki/enjoyandroid/MainActivity.kt
@@ -1,6 +1,7 @@
 package jp.yasuhiroki.enjoyandroid
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
@@ -10,11 +11,22 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.map
 import jp.yasuhiroki.enjoyandroid.ui.theme.EnjoyAndroidTheme
 
 class MainActivity : ComponentActivity() {
+
+    val src = MutableLiveData<String>("initial value is not pass to dest in lifecycle version 2.7.0")
+    val dest = src.map { it }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        dest.observe(this) {
+            Log.d("MainActivity", "dest: $it")
+        }
+        src.postValue("hello")
+
         setContent {
             EnjoyAndroidTheme {
                 // A surface container using the 'background' color from the theme
